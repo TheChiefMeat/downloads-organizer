abce7829ba481478a9fb5ac37cf002322e9eeb93
TheChiefMeat <TheChiefMeat@gmail.com>
2020-07-31 01:23:54 +0100

    Added full dir_path

--- downloadsorganizer/attributes.py
+++ downloadsorganizer/attributes.py
@@ -3,9 +3,10 @@ import ast
 
 USERNAME = os.getlogin()
 
-config_Path = open("path.config","r")
-dictionary_Path = open("dictionary.txt","r")
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
+config_Path = open(dir_path + "/path.config","r")
+dictionary_Path = open(dir_path + "/dictionary.txt","r")
 DOWNLOADS_DIRECTORY = config_Path.read()
 
 dictionary_contents = dictionary_Path.read()
